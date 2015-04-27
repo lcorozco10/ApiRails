1e7fea07807406a689222b59083db8f798c2ee3b
Luis <lcorozco10@gmail.com>
2015-04-27 17:38:29 -0600

    using migration and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,13 @@ class UsersController < ApplicationController
 
   def create
 
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :roll)
   end
 end
