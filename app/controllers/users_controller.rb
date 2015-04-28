5ef95661ad4ca08c78efdb6ece8aabd74797e6bc
Luis <lcorozco10@gmail.com>
2015-04-28 09:52:31 -0600

    Create view for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
+
+  #Get Users all
+  def index
+    @user = User.all
+  end
+
+  #Get an user by id
   def show
     @user = User.find(params[:id])
   end
-  def index
-    @articles = User.all
-  end
 
+  #Create new user
   def create
-
     @user = User.new(user_params)
-
     @user.save
     redirect_to @user
   end
+
+  #Validation Field User
   private
   def user_params
     params.require(:user).permit(:username, :password, :roll)
   end
+
 end
