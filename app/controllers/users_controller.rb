bbe68885b54dacd1da5e8bb57dc63d40084483b9
Luis <lcorozco10@gmail.com>
2015-04-29 11:16:14 -0600

    CRUD User Model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,42 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  #Create new user
+  #Form User
+  def new
+    @user = User.new
+  end
+
+  #Create new User
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  #Form user edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  #Update user
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  #Delete user
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
   #Validation Field User
