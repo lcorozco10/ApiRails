c4b66806cf01ec0fa2988715382a86c4d9ebb13b
Luis <lcorozco10@gmail.com>
2015-04-29 11:14:14 -0600

    CRUD User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  #validation for model fields http://guides.rubyonrails.org/active_record_validations.html
+  validates :username, presence: true,
+            length: { minimum: 5 }
 end
