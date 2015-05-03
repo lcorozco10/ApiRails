2c375bb32bac8b7e088c597b18120fb78eff43f9
Luis <lcorozco10@gmail.com>
2015-05-03 17:25:30 -0600

    bcrypt gem add to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   #validation for model fields http://guides.rubyonrails.org/active_record_validations.html
-  validates :username, presence: true,
-            length: { minimum: 5 }
+  validates :username, presence: true, length: { minimum: 5 }
+
+  has_secure_password
+  validates :password, presence: true, length:{minimum:6}
+
 end
---
+++ db/migrate/20150503220326_edit_column_name_to_users.rb
@@ -0,0 +1,5 @@
+class EditColumnNameToUsers < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
